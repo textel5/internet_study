f083d83cd6fb679bb14630e6ecc0fb202e1e02b3
Sergey Tokarev <sergey.tokarev.nsk@mail.ru>
2021-12-16 05:46:16 +0000

    added a licence

--- tcp_sample.h
+++ tcp_sample.h
@@ -1,3 +1,22 @@
+/*
+    tcp_sample sends (as a server) and gets (as a client) a string via tcp
+    then shuts
+    
+    Copyright (C) 2021  Sergey Tokarev
+....
+    This file is part of tcp_sample.
+    tcp_sample is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    listint is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    You should have received a copy of the GNU General Public License
+    along with tcp_sample.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 #ifndef TCP_SAMPLE_H_SENTRY
 #define TCP_SAMPLE_H_SENTRY
 #endif
